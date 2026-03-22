3c6862b67525a29a3f98d2b13f82c43dc441860b
Chris <tochrisyao@gmail.com>
2026-03-22 15:36:34 -0700

    conditional rendering (hide and if)

--- react-sandbox/src/App.jsx
+++ react-sandbox/src/App.jsx
@@ -9,20 +9,25 @@ function ScoreDisplay(props) {
   const [currentMoney, setCurrentMoney] = useState(props.money)
   useEffect(() => {
     const intervalId = setInterval(() => {
-      const randomPrice = Math.floor(Math.random() * 100) + 100
-      setCurrentMoney(randomPrice)
+
+      setCurrentMoney((latestMoney) => {
+        const randomPrice = (Math.random() * 6) - 3
+        return latestMoney + randomPrice
+      })
     }, 2000)
     return () => clearInterval(intervalId);
   }, [])
 
   const inputRef = useRef(null)
 
+  const [showDetails, setShowDetails] = useState(false)
+
 
   return (
     <div style={{ border: '1px solid white', padding: '10px', margin: '10px' }}>
       <h2>{props.text}</h2>
-      <p>${currentMoney.toFixed(2)}</p>
-      <input style={{ marginRight: '8px' }} ref={inputRef} placeholder="Buy amount..." />
+      <p style={{ color: currentMoney > props.money ? 'green' : 'red' }}>${currentMoney.toFixed(2)}</p>
+      <input style={{ marginRight: '8px' }} ref={inputRef} placeholder="Placeholder text" />
       <button onClick={() => { inputRef.current.focus() }}>Focus Input!</button>
       <div style={{ display: 'flex', gap: '8px', justifyContent: 'center', marginTop: '8px' }}>
         <button onClick={() => setCurrentMoney(currentMoney + 15)}>Pump by $15!</button>
@@ -30,6 +35,12 @@ function ScoreDisplay(props) {
         <button onClick={() => setCurrentMoney(currentMoney - currentMoney)}>Set to $0!</button>
         <button onClick={() => setCurrentMoney(props.money)}>Reset!</button>
       </div>
+      <button onClick={() => setShowDetails(!showDetails)}>Toggle Details</button>
+      {showDetails && (<div style={{ marginTop: '10px', backgroundColor: '#333', padding: '10px' }}>
+        <p>📊 52-Week High: ${props.high}</p>
+        <p>🗞️ News Sentiment: {props.news}</p>
+      </div>
+      )}
     </div>
   )
 }
@@ -42,9 +53,9 @@ function App() {
       <h1>Hello World!</h1>
       <div>
         <h1>Live Scores</h1>
-        <ScoreDisplay text="AAPL" money={194.56} />
-        <ScoreDisplay text="TSLA" money={363.54} />
-        <ScoreDisplay text="NVDA" money={193.49} />
+        <ScoreDisplay text="AAPL" money={194.56} news="Bullish" high={300.22} />
+        <ScoreDisplay text="TSLA" money={363.54} news="Bearish" high={414.48} />
+        <ScoreDisplay text="NVDA" money={193.49} news="Bearish" high={193.91} />
       </div>
 
     </>
